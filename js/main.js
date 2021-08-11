f230c77d1b73a0ed26ced3e100fc051b90291bbf
Douglas <dougls99@outlook.com>
2021-08-11 00:53:50 -0300

    modais funcionando

--- js/main.js
+++ js/main.js
@@ -1,6 +1,120 @@
-function abrir(){
-
+/* Pivo */ 
+function abrir1(){
   let pivo = document.querySelector('.pivo')
-
   pivo.style.display = 'block';
+}
+
+function fechar1(){
+  let pivo = document.querySelector('.pivo')
+  pivo.style.display = 'none';
+}
+
+/* Ponta Direito */
+function abrir2(){
+  let pontaD = document.querySelector('.pontaD')
+  pontaD.style.display = 'block';
+}
+
+function fechar2(){
+  let pontaD = document.querySelector('.pontaD')
+  pontaD.style.display = 'none';
+}
+
+/*Ponta Esquerdo*/
+function abrir3(){
+  let pontaE = document.querySelector('.pontaE')
+  pontaE.style.display = 'block';
+}
+
+function fechar3(){
+  let pontaE = document.querySelector('.pontaE')
+  pontaE.style.display = 'none';
+}
+
+/*Meia Esquerdo*/
+function abrir4(){
+  let meiaE = document.querySelector('.meiaE')
+  meiaE.style.display = 'block';
+}
+
+function fechar4(){
+  let meiaE = document.querySelector('.meiaE')
+  meiaE.style.display = 'none';
+}
+
+/*Meia Direito*/
+function abrir5(){
+  let meiaD = document.querySelector('.meiaD')
+  meiaD.style.display = 'block';
+}
+
+function fechar5(){
+  let meiaD = document.querySelector('.meiaD')
+  meiaD.style.display = 'none';
+}
+
+/*Volante*/
+function abrir6(){
+  let volante = document.querySelector('.volante')
+  volante.style.display = 'block';
+}
+
+function fechar6(){
+  let volante = document.querySelector('.volante')
+  volante.style.display = 'none';
+}
+
+/*Lateral Direito*/
+function abrir7(){
+  let lateralD = document.querySelector('.lateralD')
+  lateralD.style.display = 'block';
+}
+
+function fechar7(){
+  let lateralD = document.querySelector('.lateralD')
+  lateralD.style.display = 'none';
+}
+
+/*Lateral Esquerdo*/
+function abrir8(){
+  let lateralE = document.querySelector('.lateralE')
+  lateralE.style.display = 'block';
+}
+
+function fechar8(){
+  let lateralE = document.querySelector('.lateralE')
+  lateralE.style.display = 'none';
+}
+
+/*Zagueiro Esquerdo*/
+function abrir9(){
+  let zagueiroE = document.querySelector('.zagueiroE')
+  zagueiroE.style.display = 'block';
+}
+
+function fechar9(){
+  let zagueiroE = document.querySelector('.zagueiroE')
+  zagueiroE.style.display = 'none';
+}
+
+/*Zagueiro Direito*/
+function abrir10(){
+  let zagueiroD = document.querySelector('.zagueiroD')
+  zagueiroD.style.display = 'block';
+}
+
+function fechar10(){
+  let zagueiroD = document.querySelector('.zagueiroD')
+  zagueiroD.style.display = 'none';
+}
+
+/*Goleiro*/
+function abrir11(){
+  let goleiro = document.querySelector('.goleiro')
+  goleiro.style.display = 'block';
+}
+
+function fechar11(){
+  let goleiro = document.querySelector('.goleiro')
+  goleiro.style.display = 'none';
 }
